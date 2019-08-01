090509b404d7940c8176764fefa0d0be00fbaaac
chaytech <chen_changying@163.com>
2019-08-01 22:46:50 +0800

    SpringCloudConfig  register eureka

--- spring-cloud-config/spring-cloud-config-server/src/main/java/com/chaytech/config/SpringCloudConfigServer_Application.java
+++ spring-cloud-config/spring-cloud-config-server/src/main/java/com/chaytech/config/SpringCloudConfigServer_Application.java
@@ -3,6 +3,7 @@ package com.chaytech.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author chaytech@163.com
@@ -10,6 +11,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class SpringCloudConfigServer_Application {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudConfigServer_Application.class, args);
