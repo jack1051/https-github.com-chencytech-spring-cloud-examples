cd6f96948083bac0ae69b34e436178c52cfd59b2
chaytech <chen_changying@163.com>
2019-08-03 21:14:14 +0800

    SpringCloudConfig refresh

--- spring-cloud-config/spring-cloud-config-client/src/main/java/com/chaytech/client/ClientController.java
+++ spring-cloud-config/spring-cloud-config-client/src/main/java/com/chaytech/client/ClientController.java
@@ -1,6 +1,7 @@
 package com.chaytech.client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019/07/31 22:25
  */
 @RestController
+@RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中
 public class ClientController {
 
     @Value("${profiles}")
